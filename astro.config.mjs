76e4ab6c5ac377eaced08b207f2adb4876b30b0b
常红旭 <changhongxu@shizhuang-inc.com>
2023-08-31 16:22:47 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://retrychx.com',
+	site: 'https://retrychx.github.io',
   base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
